52fbb6399a4317b21b0713d3c8dcf1c3725c9d43
alisson2014 <almeidaalisson2014@gmail.com>
2022-11-06 18:16:19 -0300

    algumas alterações

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ function createGame(player1, hour, player2) {
   `
 }
 
-let delay = -0.5
+let delay = -0.45
 function createCard(date, day, games) {
-  delay = delay + 0.5
+  delay = delay + 0.45
   return `
   <div class="card" style="animation-delay: ${delay}s">
     <h2>${date} <span>${day}</span></h2>
